2816332f74f4ec5ba115ea21a18d9048eb284537
irvmax <38566989+irvmax@users.noreply.github.com>
2018-04-26 11:30:03 +0300

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -1,13 +1,4 @@
-#######--- Konfiguratsiooni osa ---######
-
-MQTT_HOST = "192.168.1.99"
-MQTT_seade = "klent1"
-MQTT_port = 1883
-MQTT_user = "openhabian"
-MQTT_password = "m4q22tt65"
-MQTT_sub_topic = "home/data"    # receive messages on this topic
-MQTT_pub_topic = "home/sensor/"               # send messages to this topic
-MQTT_KEEPALIVE_INTERVAL = 60
+import config.py
 
 #######--- Programmi osa ---######
 

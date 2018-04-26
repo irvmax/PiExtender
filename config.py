26087ef50fac1403120915188649d5a16dbbb2a8
irvmax <38566989+irvmax@users.noreply.github.com>
2018-04-26 11:29:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ seadmeid = 1
 
 ###### --- MQTT konfiguratsiooni osa --- ######
 
-MQTT_HOST = "192.168.1.99"
-MQTT_seade = "klent1"
+MQTT_HOST = "IP_XXX.XXX.XXX.XXX"
+MQTT_seade = "SEADE"
 MQTT_port = 1883
-MQTT_user = "openhabian"
-MQTT_password = "m4q22tt65"
+MQTT_user = "MQTT-USER"
+MQTT_password = "MQTT-PASS"
 MQTT_sub_topic = "home/data"                       # receive messages on this topic
 MQTT_pub_topic = "home/instructions"               # send messages to this topic
 MQTT_KEEPALIVE_INTERVAL = 60
